eeab0a0c1b3c46f1e8e873e5b1d3adc0c823ac49
feersantos <fmsantos1909@gmail.com>
2023-09-25 21:46:24 -0300

    origin

---
+++ Emprestimo.java
@@ -0,0 +1,20 @@
+package Financiamento;
+
+import java.util.*;
+
+public class Emprestimo {
+
+	public static void main(String[] args) {
+		
+		Scanner emp = new Scanner(System.in);
+		
+		System.out.print("Valor: ");
+		double total = emp.nextDouble();
+		System.out.print("Taxa anual: ");
+		double taxa = emp.nextDouble();
+		System.out.print("Tempo (anos): ");
+		int n = emp.nextInt();
+		
+	}
+
+}
---
+++ InfoEmprestimo.java
@@ -0,0 +1,9 @@
+package Financiamento;
+
+public class InfoEmprestimo {
+
+	static double total; //valor do emprestimo
+	static double taxa; //valor da taxa
+	static int n; //duração
+	
+}
